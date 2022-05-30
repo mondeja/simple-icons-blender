efc4502df82b506125961158b6a1aab4f7e3e461
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-05-30 10:55:08 +0200

    Refactor to ESM and optimize addon size (#6)
    * Move to ESM
    
    * Fix error in CI
    
    * Revert change
    
    * Fix error in pytest config
    
    * Reduce testing verbosity
    
    * Optimize size

--- build.js
+++ build.js
@@ -1,27 +1,28 @@
-const fs = require("fs");
-const simpleIcons = require("simple-icons");
+import fs from "node:fs";
+import * as icons from "simple-icons/icons";
 
-const simpleIconsArray = Object.keys(simpleIcons);
+const simplifyHexIfPossible = (hex) => {
+  if (hex[0] === hex[1] && hex[2] === hex[3] && hex[4] == hex[5]) {
+    return `${hex[0]}${hex[2]}${hex[4]}`;
+  }
+  return hex;
+};
 
 let simpleIconsClassesString = '';
-for (let slug of simpleIconsArray) {
-  const icon = simpleIcons[slug];
+for (let si in icons) {
+  const icon = icons[si];
   const styledSvg = icon.svg
     .replace(/'/g, "\\'")
-    .replace('<path d="', `<path fill="#${icon.hex}" d="`);
+    .replace('<path d="', `<path fill="#${simplifyHexIfPossible(icon.hex)}" d="`);
   const escapedTitle = icon.title.replace(/'/g, "\\'");
 
-  simpleIconsClassesString += `class AddSi_${slug}(AddSi, bpy.types.Operator):
-    bl_idname = "mesh.si_${slug}"
-    bl_label = '${escapedTitle}'
-    bl_description = 'Add ${escapedTitle} brand icon'
-    si_svg = '${styledSvg}'
+  simpleIconsClassesString += `class AddSi_${icon.slug}(A,O):bl_idname="mesh.si_${icon.slug}";bl_label='${escapedTitle}';bl_description='Add ${escapedTitle} brand icon';si_svg='${styledSvg}'
 `
 }
 
 const template = fs.readFileSync("simple_icons_blender.template").toString();
 let newInitContent =
-  `# This file has been created automatically, don't edit it!\n\n` +
+  `# This file has been generated\n\n` +
   template.replace(
     "%(simple_icons_classes)s", simpleIconsClassesString
   );
